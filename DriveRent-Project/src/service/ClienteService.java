48ed940aed63082006b80aec08fb2995e5545215
lazaroaraujo-dev <lazarojuan08.dev@gmail.com>
2026-08-17 01:01:27 -0300

    refactor: extract validation methods and implement atualizar in ClienteService
    - Extracted CPF and Name validation logic into private helper methods (validarCpf, validarNome) to avoid code duplication.
    
    - Implemented atualizar method with standard validations.
    
    - Added EntidadeNaoEncontradaException handling for update operations on non-existing records.

--- DriveRent-Project/src/service/ClienteService.java
+++ DriveRent-Project/src/service/ClienteService.java
@@ -2,6 +2,7 @@ package service;
 
 import dao.PersistenciaDao;
 import exception.DadosInvalidosException;
+import exception.EntidadeNaoEncontradaException;
 import model.entities.Cliente;
 
 public class ClienteService {
@@ -15,21 +16,42 @@ public class ClienteService {
         if (cliente==null){
             throw new DadosInvalidosException("Os dados do cliente não podem ser nulos.");
         }
-
         // Validação do CPF
 
-        if (cliente.getCpf() == null || cliente.getCpf().isEmpty()){
-            throw new DadosInvalidosException("O CPF do cliente é obrigatório.");
-        }
-        if (cliente.getCpf().length() != 11 || !cliente.getCpf().matches("\\d+")){
-            throw new DadosInvalidosException("O CPF deve conter exatamente 11 números!");
-        }
-        if (cliente.getNome() == null || cliente.getNome().isEmpty()){
-            throw new DadosInvalidosException("O nome do cliente é obrigatório.");
-        }
+        validarCpf(cliente.getCpf());
+
+        validarNome(cliente.getNome());
+
         if (clienteDao.buscarPorId(cliente.getCpf()) != null){
             throw new DadosInvalidosException("Já existe um cliente cadastrado com este CPF.");
         }
         clienteDao.salvar(cliente);
+    }
+
+    public void atualizar(Cliente clienteAtualizado){
+        if (clienteAtualizado == null){
+            throw new DadosInvalidosException("Os dados do cliente não podem ser nulos");
+        }
+        validarCpf(clienteAtualizado.getCpf());
+
+        if (clienteDao.buscarPorId(clienteAtualizado.getCpf()) == null){
+            throw new EntidadeNaoEncontradaException("O clienteAtualizado não possui cadastro no sistema.");
+        }
+        validarNome(clienteAtualizado.getNome());
+
+        clienteDao.atualizar(clienteAtualizado);
+    }
+    private void validarCpf(String cpf){
+        if (cpf == null || cpf.isEmpty()){
+            throw new DadosInvalidosException("O CPF do cliente é obrigatório.");
+        }
+        if (cpf.length() != 11 || !cpf.matches("\\d+")){
+            throw new DadosInvalidosException("O CPF deve conter exatamente 11 números!");
+        }
+    }
+    private void validarNome(String nome){
+        if (nome == null || nome.isEmpty()){
+            throw new DadosInvalidosException("O nome do cliente é obrigatório.");
+        }
     }
 }
